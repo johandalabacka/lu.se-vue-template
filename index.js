d7511098c44253c101de889bcced495cd3c44cd2
Johan Dahl <johan.dalabacka@gmail.com>
2019-08-18 19:08:40 +0200

    export components via index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import LuNavMobile from './LuNavMobile'
+import LuHeader from './LuHeader'
+import LuBreadCrumb from './LuBreadCrumb'
+import LuMain from './LuMain'
+import LuFooter from './LuFooter'
+
+export {
+    LuNavMobile,
+    LuHeader,
+    LuBreadCrumb,
+    LuMain,
+    LuFooter
+}
